2d3449112d6349af4a49a3e124f46faac2788bf4
BristonLowell <bristonlowell@gmail.com>
2020-11-03 16:19:11 -0700

    added a slap button

--- src/components/Apod.vue
+++ src/components/Apod.vue
@@ -6,7 +6,7 @@
     </h1>
   </div>
   <div class="apod col-2 mt-5">
-    <form @submit.prevent="getApod()">
+    <form @submit.prevent="getApod()" id="billy-form">
       <label for="apod"></label>
       <input type="text" class="form-control" name="apod" placeholder="Date: DD-MM-YYYY" v-model="state.date">
       <button class="btn btn-primary btn-block" type="submit">
@@ -25,13 +25,21 @@
       </div>
     </div>
   </div>
-  <div class="col-12 d-flex" v-else>
+  <div class="col-12 d-flex justify-content-center" v-else>
     <div class="row my-5">
-      <div class="col-10">
+      <div class="col-6">
         <img src="https://i.pinimg.com/originals/53/04/c5/5304c56282c143abc861cefc10daa4f7.jpg" class="img-fluid" alt="">
       </div>
-      <div class="col-2 text-left">
-        <p>This is Billy. Billy slays.</p>
+      <div class="col-5 text-center">
+        <p id="billy" class="text-danger">
+          This is Billy. Billy slays.
+        </p>
+        <h1 class="p-1">
+          Health: {{ state.health }}
+        </h1>
+        <button id="billy-button" @click="slapBilly()" class="btn btn-danger">
+          Slap Billy
+        </button>
       </div>
     </div>
   </div>
@@ -45,10 +53,40 @@ export default {
   name: 'Apod',
   setup() {
     const state = reactive({
-      date: ''
+      date: '',
+      health: 100
     })
     return {
       state,
+      slapBilly() {
+        state.health--
+        if (state.health <= 0) {
+          state.health = 0
+          alert('Wow you really killed Billy...')
+        }
+        if (state.health <= 90 && state.health > 70) {
+          document.getElementById('billy').innerText = "You're making me angry!"
+        }
+        if (state.health <= 70 && state.health > 50) {
+          document.getElementById('billy').innerText = "I'll disable your functions!"
+        }
+        if (state.health <= 50 && state.health > 40) {
+          document.getElementById('billy').innerText = 'I tried to warn you'
+          document.getElementById('billy-form').classList.add('d-none')
+        }
+        if (state.health <= 40 && state.health > 37) {
+          document.getElementById('billy').innerText = 'Good luck hitting me now'
+        }
+        if (state.health <= 37 && state.health > 15) {
+          document.getElementById('billy').innerText = 'HAHAHAHAHAHAHAHA!!!'
+          document.getElementById('billy-button').classList.add('bg-white')
+          document.getElementById('billy-button').classList.add('border-white')
+        }
+        if (state.health <= 15) {
+          document.getElementById('billy').innerText = 'please no :('
+          document.getElementById('billy-form').classList.remove('d-none')
+        }
+      },
       apod: computed(() => AppState.apod),
       getApod() {
         const date = state.date.split('-')
